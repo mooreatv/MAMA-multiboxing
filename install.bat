5288fc244d5f2173354edb800d79a715a4a52c2f
Moorea Tv <moorea@ymail.com>
2021-05-10 19:46:39 -0700

    Multi TOC support ready for Classic, Classic Era, Burning Crusade and Shadowlands

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-for %%x in (_retail_ _classic_ _ptr_) do (
+for %%x in (_retail_ _classic_ _ptr_ _classic_ptr_) do (
 echo Installing for %%x
 xcopy /i /y Mama\*.* "C:\Program Files (x86)\World of Warcraft\%%x\Interface\Addons\Mama"
 )
