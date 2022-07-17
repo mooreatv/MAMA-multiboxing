bfdabc832f88ff5542b82774d08834616e536d1d
Moorea Tv <moorea@ymail.com>
2022-07-17 13:06:06 -0700

    wrath classic

--- install.bat
+++ install.bat
@@ -1,4 +1,4 @@
-for %%x in (_retail_ _classic_ _classic_era_ legacy) do (
+for %%x in (_retail_ _classic_ _classic_era_ _classic_beta_ legacy) do (
 echo Installing for %%x
 xcopy /i /y Mama\*.* "C:\Program Files (x86)\World of Warcraft\%%x\Interface\Addons\Mama"
 )
